20f11222eff19122018ba006bfcb75402d13f449
Laica Lunasys <laica.lunasys@gmail.com>
2019-06-28 19:18:03 +0900

    Fix extraction problem. maybe.

--- systerapb/make-java.sh
+++ systerapb/make-java.sh
@@ -9,7 +9,7 @@ mkdir -p $EXPORTDIR
 cp -Rfv $BASEMENT/*.proto $EXPORTDIR/
 cd java
 if [ "$1" == "deploy" ]; then
-    mvn clean deploy -DrepositoryId=synchthia-repo -X
+    mvn clean deploy
 else
     mvn clean install
 fi
