23bc2342dbc32985762c5873343e8946649afaa8
Laica Lunasys <laica.lunasys@gmail.com>
2019-06-28 18:41:28 +0900

    Add debug option

--- systerapb/make-java.sh
+++ systerapb/make-java.sh
@@ -9,7 +9,7 @@ mkdir -p $EXPORTDIR
 cp -Rfv $BASEMENT/*.proto $EXPORTDIR/
 cd java
 if [ "$1" == "deploy" ]; then
-    mvn clean deploy -DrepositoryId=synchthia-repo
+    mvn clean deploy -DrepositoryId=synchthia-repo -X
 else
     mvn clean install
 fi
