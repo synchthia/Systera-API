e30886e819077c25d0255108026375177d99881f
Laica Lunasys <laica.lunasys@gmail.com>
2019-06-28 18:14:00 +0900

    Export maven deploy script to .ci directory

---
+++ .ci/extract-maven-settings.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "$(dirname "$0")"/..
+
+if [ ! -e $HOME/.m2/ ]; then
+    mkdir $HOME/.m2/
+fi
+
+if [ ! -e $PWD/.env ]; then
+    echo "!! .env does not exists!"
+    exit 1
+fi
+source .env
+echo -n ${CI_MVN_SETTINGS} > $HOME/.m2/settings.xml
